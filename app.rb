677cc583343b63210554c5366d9e6fa25b9b5cc3
wlytle <wlytle89@gmail.com>
2020-10-25 21:31:05 -0700

    commit tree 603e3620b025b8ecf5088079ad5ad71041eb1e3a

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
 
   # This is a sample static route.
-  get '/hello' do
+  get "/hello" do
     "Hello World!"
   end
 
@@ -14,5 +14,14 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
 
+  get "/multiply/:num_1/:num_2" do
+    @num_1 = params[:num_1].to_i
+    @num_2 = params[:num_2].to_i
+    "The Prodcut is: #{@num_1 * @num_2}"
+  end
 end
